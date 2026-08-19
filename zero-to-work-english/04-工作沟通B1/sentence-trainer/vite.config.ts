03a625c95f76869ff60cf6d930205f80ac7d1432
Carter Zheng <carter.zheng@globalpay.com>
2026-08-19 23:17:19 +0800

    support root local production preview

--- zero-to-work-english/04-工作沟通B1/sentence-trainer/vite.config.ts
+++ zero-to-work-english/04-工作沟通B1/sentence-trainer/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/learn-english/',
+  base: process.env.GITHUB_ACTIONS ? '/learn-english/' : '/',
   plugins: [
     react(),
     VitePWA({
